cffa80b523d984a26cf6d4f70664d9be242e1c52
Aurélien Gasser <aurelien.gasser@owkin.com>
2019-09-22 21:46:33 -0400

    display article metadata + article css

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ require 'redcarpet'
 require_relative '../helpers/custom_render'
 
 class Article
-    attr_accessor :id, :language, :title, :html_content, :html_content_short
+    attr_accessor :id, :language, :source, :original_title, :title, :author, :public_notes_html, :content_html, :content_html_short
     
     @@markdown = Redcarpet::Markdown.new(CustomRender, fenced_code_blocks: true)
     @id = ''
@@ -11,9 +11,11 @@ class Article
     @original_title = ''
     @title = ''
     @author = ''
-    @public_notes = ''
-    @html_content
-    @html_content_short
+    @public_notes_md = ''
+    @public_notes_html = ''
+    @content_html
+    @content_html_short
+    @content_md = ''
 
     def initialize(article_path)
         mdocd_content = IO.read(article_path)
@@ -23,7 +25,7 @@ class Article
 
     def populate_from_mocd(mdocd_content)
         parts = mdocd_content.split('===')
-        @md_raw = parts[1]
+        @content_md = parts[1]
         lines = parts[0].split("\n")
         lines.each do |line|
             if line.start_with?('Id: ')
@@ -39,15 +41,18 @@ class Article
             elsif line.start_with?('Author: ')
                 @author = line[8..-1]
             elsif line.start_with?('Public notes: ')
-                @public_notes = line[14..-1]
+                @public_notes_md = line[14..-1]
             end
         end
     end
 
     def convert_to_html
-        @html_content = @@markdown.render(@md_raw).html_safe
-        md_raw_short = @md_raw.split("\n\n")[0..4].join("\n\n")
-        @html_content_short = @@markdown.render(md_raw_short).html_safe
+        content_md_short = @content_md.split("\n\n")[0..4].join("\n\n")
+        @content_html = @@markdown.render(@content_md).html_safe
+        @content_html_short = @@markdown.render(content_md_short).html_safe
+        if (@public_notes_md)
+            @public_notes_html = @@markdown.render(@public_notes_md).html_safe
+        end
     end
 
     def to_s
@@ -57,9 +62,9 @@ class Article
         "Original Title: #{@original_title}\n"\
         "Title: #{@title}\n"\
         "Author: #{@author}\n"\
-        "Public Notes: #{@public_notes}\n"\
+        "Public Notes: #{@public_notes_md}\n"\
         "Raw MD: #{@md_raw.length} characters\n"\
-        "HTML: #{@html_content.length} characters\n"\
-        "HTML (short): #{@html_content_short.length} characters\n"
+        "HTML: #{@content_html.length} characters\n"\
+        "HTML (short): #{@content_html_short.length} characters\n"
     end
 end
