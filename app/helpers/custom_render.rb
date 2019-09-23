8877beaac6c8a9df1b4a0a499ad686dc7a2e2f3d
Aurélien Gasser <aurelien.gasser@owkin.com>
2019-09-23 19:57:58 -0400

    more styling stuff

--- app/helpers/custom_render.rb
+++ app/helpers/custom_render.rb
@@ -1,5 +1,5 @@
 class CustomRender < Redcarpet::Render::HTML
     def link(link, title, content)
-        "<a href=\"#{link}\" class=\"external-link\" target=\"_blank\">#{content}</a>"
+        "<a href=\"#{link}\" target=\"_blank\">#{content}</a>"
     end
 end
