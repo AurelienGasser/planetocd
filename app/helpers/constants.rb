5ae44396c1f1c70db47d499a3601b9652a4039ed
Aurélien Gasser <aurelien.gasser@owkin.com>
2019-09-30 01:44:02 -0400

    about page

--- app/helpers/constants.rb
+++ app/helpers/constants.rb
@@ -1,4 +1,6 @@
 module Constants
     GITHUB = "https://github.com/momow/planetocd"
     SITE_NAME = "Planet OCD"
+    EMAIL = "aurelien.ocd@gmail.com"
+    DONATE_URL = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=23LG7JTZSCA54"
 end
