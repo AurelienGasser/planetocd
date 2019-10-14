5b3a0b116401deb1a0524e57530befb4f4407fb9
Aurélien Gasser <aurelien.gasser@owkin.com>
2019-10-14 19:55:49 -0400

    fix link

--- app/helpers/constants.rb
+++ app/helpers/constants.rb
@@ -1,6 +1,6 @@
 module Constants
     GITHUB = "https://github.com/momow/planetocd"
-    GITHUB_CONTRIBUTE = "https://github.com/momow/planetocd/CONTRIBUTE.md"
+    GITHUB_CONTRIBUTE = GITHUB + "/blob/master/CONTRIBUTE.md"
     SITE_NAME = "Planet OCD"
     EMAIL = "aurelien.ocd@gmail.com"
     DONATE_URL = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=23LG7JTZSCA54"
