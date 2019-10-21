95d6b93d8dddaed6af99f1532174599316c5a2a8
Aurélien Gasser <aurelien.gasser@owkin.com>
2019-10-21 17:05:14 -0400

    add social links

--- app/helpers/constants.rb
+++ app/helpers/constants.rb
@@ -1,6 +1,8 @@
 module Constants
     GITHUB = "https://github.com/momow/planetocd"
     GITHUB_CONTRIBUTE = GITHUB + "/blob/master/README.md#contribute"
+    FACEBOOK = "http://facebook.com/PlanetOCD.francais"
+    TWITTER = "https://twitter.com/PlanetOCD_fr"
     SITE_NAME = "Planet OCD"
     EMAIL = "aurelien.ocd@gmail.com"
     DONATE_URL = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=23LG7JTZSCA54"
