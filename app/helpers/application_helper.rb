7c3f727900c7858b3313879b131cf9735f9bfc4d
Aurélien Gasser <aurelien.gasser@owkin.com>
2019-11-03 18:57:44 -0500

    localized images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def localize_file(filename)
+        idx = filename.rindex('.')
+        if idx.nil?
+            return filename
+        end
+        suffix = "_" + I18n.locale.to_s
+        return filename[0..idx-1] + suffix + filename[idx..-1]
+    end
 end
