38996f95b700f9a459f4863b30c80e9cdd72c971
Aurélien Gasser <aurelien.gasser@gmail.com>
2021-06-21 01:28:34 -0400

    add article footer prompts

--- server/page.go
+++ server/page.go
@@ -1,6 +1,11 @@
 package server
 
-import "net/url"
+import (
+	"html/template"
+	"net/url"
+	"regexp"
+	"strings"
+)
 
 type page struct {
 	Constants map[string]interface{}
@@ -33,4 +38,17 @@ func (p *page) URL(path string) string {
 // MustGetURL returns the URL for a name, and panics if not found
 func (p *page) MustGetURL(name string) *url.URL {
 	return mustGetURL(name, p.Meta.Lang)
+}
+
+func (p *page) ReplaceURL(s, old, url string) template.HTML {
+	return template.HTML(strings.Replace(s, old, "<a href=\""+template.HTMLEscapeString(url)+"\">"+old+"</a>", 1))
+}
+
+func (p *page) ReplaceURLTemplate(s template.HTML, old, url string) template.HTML {
+	return p.ReplaceURL(string(s), old, url)
+}
+
+func (p *page) ReplaceEmail(s string) template.HTML {
+	re := regexp.MustCompile(`(.*)\[(.*)\]\(#email#\)(.*)`)
+	return template.HTML(re.ReplaceAll([]byte(s), []byte("$1<a href=\"mailto:"+template.HTMLEscapeString(Email)+"\">$2</a>$3")))
 }
