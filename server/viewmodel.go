a0d94821cc7c9129ddd52c0a09f0f784e10d684e
Aurélien Gasser <aurelien.gasser@gmail.com>
2021-12-08 20:40:52 -0500

    use small images in thumnails

--- server/viewmodel.go
+++ server/viewmodel.go
@@ -3,8 +3,10 @@ package server
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/url"
 	"regexp"
+	"strings"
 )
 
 type ViewModel struct {
@@ -58,4 +60,13 @@ func (p *ViewModel) ReplaceURLPatternTemplate(s template.HTML, needle string, ur
 
 func (p *ViewModel) Tag(tag string) template.HTML {
 	return template.HTML(fmt.Sprintf("<span class=\"uk-label uk-label-success\">%v</span>", p.T(fmt.Sprintf("tag_%v", tag))))
+}
+
+func (p *ViewModel) SmallImage(u *url.URL) *url.URL {
+	res, err := url.Parse(strings.Replace(u.String(), "_md.", "_sm.", 1))
+	if err != nil {
+		log.Printf("Error: unable to get small image for %v\n", u)
+		return u
+	}
+	return res
 }
